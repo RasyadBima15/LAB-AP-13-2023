896f65bd816f1059b684ff27e4e02f7567e76078
ivy799 <muhammadraihanaan123@gmail.com>
2023-09-13 21:23:13 +0800

    Tugas Praktikum-1

---
+++ H071231078/praktikum-1/TP1_4_H071231078.py
@@ -0,0 +1,7 @@
+print("pengujian jenis karakter")
+print("------------------------")
+
+x= input("karakter = ")
+print("Huruf kapital ? ",x.isupper())
+print("Huruf kecil ? ",x.islower())
+print("angka ? ",x.isdigit())
