3e57d4402153d64ad77d0022c73cd2818bbc6e72
FITRIANIJAYA <fitriani.jaya@gmail.com>
2023-09-30 13:09:52 +0800

    tugas praktikum-3

--- H071231012/Praktikum-3/TP3_2_H071231012.py
+++ H071231012/Praktikum-3/TP3_2_H071231012.py
@@ -5,8 +5,7 @@ kembalian = jumlah_uang - harga_barang
 
 pecahan_uang = [100000,50000,20000,10000,5000,2000,1000]
 
-for i in range(1,5):
-    #jumlah_pecahan = kembalian // pecahan #bilangan bulat
-    #kembalian %= pecahan
-    #print(f'{jumlah_pecahan} uang sejumlah Rp. {pecahan}')
-    print(i)
+for pecahan in pecahan_uang:
+    jumlah_pecahan = kembalian // pecahan #bilangan bulat
+    kembalian %= pecahan
+    print(f'{jumlah_pecahan} uang sejumlah Rp. {pecahan}')
