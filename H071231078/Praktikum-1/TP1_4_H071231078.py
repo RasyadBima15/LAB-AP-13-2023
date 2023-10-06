c5c2a37eb091bc6b11c30b7ac5dabe94b4c132ae
nabilmuflif <145102114+nabilmuflif@users.noreply.github.com>
2023-10-06 21:16:07 +0800

    Delete H071231078 directory

--- H071231078/Praktikum-1/TP1_4_H071231078.py
+++
@@ -1,7 +0,0 @@
-print("pengujian jenis karakter")
-print("------------------------")
-
-x= input("karakter = ")
-print("Huruf kapital ? ",x.isupper())
-print("Huruf kecil ? ",x.islower())
-print("angka ? ",x.isdigit())
